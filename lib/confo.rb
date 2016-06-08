4d5be81947b450c6daa329bcc111096ae629cf46
Yaroslav Konoplov <yaroslav@inbox.com>
2016-06-08 14:51:59 +0300

    ruby 2.3 frozen string literal

--- lib/confo.rb
+++ lib/confo.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'active_support/all'
 require 'confo/version'
 require 'confo/concerns/options_manager'
--- lib/confo/version.rb
+++ lib/confo/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Confo
   VERSION = '1.0.1'
 end
